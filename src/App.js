3bb9e61bb87133dd6a053b28451c1eae471cdebc
BigBOii <d.barlow@laposte.net>
2022-05-03 16:46:29 +0200

    :construction: Main view added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./components/Home";
+import Private from "./components/Private";
+import Messages from "./components/Messages";
+import Explore from "./components/Explore";
+import Profile from "./components/Profile";
+import Sidebar from "./components/Sidebar";
+import Bookmarks from "./components/Bookmarks";
+import Notifications from "./components/Notifications";
+import List from "./components/List";
+const App = () => {
+    return(
+       <div className="app">
+            <BrowserRouter>
+            <Sidebar/>
+                <Routes>  {/* this is the router */}
+                    <Route path="/home" exact element={<Home/>}/>
+                    <Route path="*" exact element={<Home/>}/>
+                    <Route path="/messages" exact element={<Messages/>}/>
+                    <Route path="/explore" element={<Explore/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+                    <Route path="/private" exact element={<Private/>}/>
+                    <Route path="/bookmarks" exact element={<Bookmarks/>}/>
+                    <Route path="/notifications" element={<Notifications/>}/>
+                    <Route path="/lists" element={<List/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
-
 export default App;
