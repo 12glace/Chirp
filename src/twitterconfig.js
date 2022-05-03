0f5f019440bb261201bb00c1e54c27dc630c7fe7
BigBOii <d.barlow@laposte.net>
2022-05-03 16:48:52 +0200

    :construction: Twitter config for api

---
+++ src/twitterconfig.js
@@ -0,0 +1,12 @@
+export default global.config = {
+    // options : {
+    //     method: 'GET',
+    //     url: 'https://twitter135.p.rapidapi.com/Search/',
+    //     params: {q: 'Dogecoin', count: '20'},
+    //     headers: {
+    //       'X-RapidAPI-Host': 'twitter135.p.rapidapi.com',
+    //       'X-RapidAPI-Key': '63ba1ff21fmshe6a53ea2221dbdap1eb2afjsn723d6f406406'
+    //     }
+    // }
+    
+};
